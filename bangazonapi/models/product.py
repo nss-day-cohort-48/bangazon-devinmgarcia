f5d55faa8fae89777c81286934aa2f5e20207979
Devin Garcia <devinmgarcia@gmail.com>
2021-08-30 10:23:04 -0500

    line 65 in product.py. added a check to prevent division by zero error

--- bangazonapi/models/product.py
+++ bangazonapi/models/product.py
@@ -61,8 +61,8 @@ class Product(SafeDeleteModel):
         total_rating = 0
         for rating in ratings:
             total_rating += rating.rating
-
-        avg = total_rating / len(ratings)
+        if len(ratings):
+            avg = total_rating / len(ratings)
         return avg
 
     class Meta:
