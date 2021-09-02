ec8e0fa2aabbf0e750d3fb67318cd06bdd5a13aa
Devin Garcia <devinmgarcia@gmail.com>
2021-09-02 14:49:24 -0500

    completed order report

--- bangazonreports/views/__init__.py
+++ bangazonreports/views/__init__.py
@@ -1,3 +1,5 @@
 from .connection import Connection
 from .users.favoritedbycustomer import favoritedbycustomer_list
 from .orders.incompleteorders import incompleteorder_list
+from .orders.completedorders import completeorder_list
+
