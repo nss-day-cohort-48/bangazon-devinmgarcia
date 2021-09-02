735f1a401c4c9eb0854bd662dfda4315ddb205a4
Devin Garcia <devinmgarcia@gmail.com>
2021-09-02 15:47:21 -0500

    expensive products

--- bangazonreports/urls.py
+++ bangazonreports/urls.py
@@ -2,11 +2,12 @@ from bangazonreports.views.products.inexpensiveproducts import inexpensiveproduc
 from bangazonreports.views.orders.incompleteorders import incompleteorder_list
 from bangazonreports.views.orders.completedorders import completeorder_list
 from django.urls import path
-from .views import favoritedbycustomer_list, incompleteorder_list, inexpensiveproduct_list, completeorder_list
+from .views import favoritedbycustomer_list, incompleteorder_list, inexpensiveproduct_list, completeorder_list, expensiveproduct_list
 
 urlpatterns = [
     path('reports/favoritesellers', favoritedbycustomer_list),
     path('reports/incompleteorders', incompleteorder_list),
     path('reports/inexpensiveproducts', inexpensiveproduct_list),
+    path('reports/expensiveproducts', expensiveproduct_list),
     path('reports/completeorders', completeorder_list),
 ]
