01e8d1d82314fa7b5a7a0dd029ef79310c1f7b9e
Devin Garcia <devinmgarcia@gmail.com>
2021-09-02 11:26:36 -0500

    adding constraint

--- bangazonapi/models/customerproductlike.py
+++ bangazonapi/models/customerproductlike.py
@@ -3,3 +3,5 @@ from django.db import models
 class CustomerProductLike(models.Model):
     customer = models.ForeignKey("Customer", on_delete=models.DO_NOTHING)
     product = models.ForeignKey("Product", on_delete=models.CASCADE, related_name="likes")
+    class Meta:
+        unique_together = ('customer', "product")
